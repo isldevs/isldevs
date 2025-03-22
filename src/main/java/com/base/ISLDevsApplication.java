5bf42d48913a47340b118b9767120accead825ba
iSLDevs <sivlayyi.bs@gmail.com>
2025-03-22 11:56:19 +0700

    init commit.

---
+++ src/main/java/com/base/ISLDevsApplication.java
@@ -0,0 +1,13 @@
+package com.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISLDevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISLDevsApplication.class, args);
+	}
+
+}
