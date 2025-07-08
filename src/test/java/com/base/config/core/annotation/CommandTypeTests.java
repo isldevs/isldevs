128546611d25dbdf191178417efbc675696b35dd
iSLDevs <sivlayyi.bs@gmail.com>
2025-07-08 16:37:00 +0700

    Added annotation class to handle entity action.

---
+++ src/test/java/com/base/config/core/annotation/CommandTypeTests.java
@@ -0,0 +1,11 @@
+package com.base.config.core.annotation;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommandTypeTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
