c937fb506be9f7d0a435f2b7526346bf239a5599
iSLDevs <sivlayyi.bs@gmail.com>
2025-08-17 23:24:30 +0700

    Updated Header License on files.

---
+++ src/test/java/com/base/config/serialization/JsonHelperTests.java
@@ -0,0 +1,11 @@
+package com.base.config.serialization;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonHelperTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
