4924c536b231979a6e6db698e3c9a42d2ede4188
iSLDevs <sivlayyi.bs@gmail.com>
2025-08-25 23:23:55 +0700

    feat(auth): update jwt authentication converter

--- src/main/java/com/base/config/security/converter/CustomJwtAuthenticationConverter.java
+++ src/main/java/com/base/config/security/converter/CustomJwtAuthenticationConverter.java
@@ -17,7 +17,6 @@ package com.base.config.security.converter;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -50,11 +49,7 @@ public class CustomJwtAuthenticationConverter implements Converter<Jwt, Abstract
             return new JwtAuthenticationToken(jwt, authorities, principalName);
         } catch (Exception e) {
             log.error("Failed to convert JWT to Authentication", e);
-            return new UsernamePasswordAuthenticationToken(
-                    jwt.getSubject(),
-                    "N/A",
-                    Collections.emptyList()
-            );
+            return new JwtAuthenticationToken(jwt, Collections.emptyList(), null);
         }
     }
 
