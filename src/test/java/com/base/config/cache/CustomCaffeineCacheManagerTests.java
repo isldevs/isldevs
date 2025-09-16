f1d60d306a68c020beaf06899dc974f27fdc8f02
iSLDevs <sivlayyi.bs@gmail.com>
2025-09-16 11:01:38 +0700

    feat(cache): update cache configuration.

---
+++ src/test/java/com/base/config/cache/CustomCaffeineCacheManagerTests.java
@@ -0,0 +1,11 @@
+package com.base.config.cache;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomCaffeineCacheManagerTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
